03fa1eaad5a3d60229054059117f26f4728fb9c8
Luismarin2510 <opraedu@gmail.com>
2023-02-15 20:13:00 -0500

    first commit

---
+++ src/variables/caracteres.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class caracteres {
+
+	public static void main(String[] args) {
+//////////////// char ///////////////////////
+///////// char solo soporta un caracter a alavez //////////
+
+		char caracter = 'a'; // dbe ser en comillas simples
+		System.out.println(caracter);
+
+		caracter = 55; // char toma la posicion de la tabla ascii
+		System.out.println(caracter);
+
+///////////////////////// string/////////////////////////////
+
+		String palabra = "Alura cursos online"; // usa cadena de textos
+		System.out.println(palabra);
+
+		palabra = palabra + " 2023";
+		System.out.println(palabra);
+
+	}
+
+}
