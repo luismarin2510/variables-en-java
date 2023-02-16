42d6b13f1b71a1edc91376efb38a68885bac359d
Luismarin2510 <opraedu@gmail.com>
2023-02-15 20:16:05 -0500

    condicionales

---
+++ src/variables/condicionales.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class condicionales {
+
+	public static void main(String[] args) {
+
+		int edad = 29;
+
+		if (edad >= 30) {
+			System.out.println("si eres aceptado");
+		} else {
+			System.out.println("No eres aceptado");
+		}
+
+	}
+
+}
