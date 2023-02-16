fe9ef79974097326808f11a6479e1105de2ae629
Luismarin2510 <opraedu@gmail.com>
2023-02-15 20:21:24 -0500

    tablas de multiplicar

---
+++ src/variables/ejercicio_tablas_multiplicar.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class ejercicio_tablas_multiplicar {
+
+	public static void main(String[] args) {
+
+		int tabla = 1;
+
+		for (int contador = 0; contador <= 10; contador++) {
+			System.out.println(tabla + "X" + contador + "=" + (tabla * contador));
+
+		}
+
+	}
+
+}
