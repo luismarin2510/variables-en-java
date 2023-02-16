029a09e1f288b9d345873fd4a6c73642614e96eb
Luismarin2510 <opraedu@gmail.com>
2023-02-15 20:31:58 -0500

    ejercicio factorial

---
+++ src/variables/factorial.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class factorial {
+
+	public static void main(String[] args) {
+
+		int numero = 2;
+
+		for (int Factorial = 1; Factorial <= 4; Factorial++) {
+			numero *= Factorial;
+			System.out.println("el factorial es " + numero + " de " + Factorial);
+		}
+
+	}
+
+}
